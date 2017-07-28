5f75327a79684daed8881757694bb09ce2ae1502
Ollie Baker <oliverjohnbaker@gmail.com>
2017-07-28 12:06:45 +0100

    adding password reset form

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,4 +1,7 @@
 const passport = require('passport');
+const mongoose  = require('mongoose');
+const User = mongoose.model('User');
+const crypto = require('crypto');
 
 exports.login = passport.authenticate('local', {
   failureRedirect: '/login',
@@ -22,3 +25,20 @@ exports.isLoggedIn = (req, res, next) => {
   res.redirect('/login');
 };
 
+exports.forgot = async(req, res, next) => {
+
+  const user = await User.findOne({ email: req.body.email});
+  if(!user) {
+    req.flash('error', 'No email found'); //dont do this in real life!
+    return res.redirect('/login');
+  }
+
+  user.resetPasswordToken = crypto.randomBytes(20).toString('hex');
+  user.resetPasswordExpires = Date.now() + 3600000;
+  await user.save();
+
+  const resetURL = `http://${req.headers.host}/account/reset/${user.resetPasswordToken}`
+  req.flash('success', `You have been sent a password reset link ${resetURL}`);
+
+  res.redirect('/login');
+}
