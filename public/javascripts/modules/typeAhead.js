0a897442188c7e359ea0f9602c89a78a2a4b35d6
Ollie Baker <oliverjohnbaker@gmail.com>
2017-08-03 15:49:59 +0100

    adding typeAhead feature and fixing .gitignore

---
+++ public/javascripts/modules/typeAhead.js
@@ -0,0 +1,76 @@
+const axios = require('axios');
+import dompurify from 'dompurify';
+
+function searchResultsHTML(stores) {
+  return stores.map( store => {
+    return `
+    <a href="/store/${store.slug}" class="search__result">
+      <strong>${store.name}</strong>
+    </a>
+  `;
+  }).join('');
+}
+
+function typeAhead(search) {
+  if(!search) return;
+
+  const searchInput = search.querySelector('input[name="search"]');
+  const searchResults = search.querySelector('.search__results');
+
+  console.log(searchInput, searchResults);
+
+  searchInput.on('input', function() {
+    if(!this.value) {
+      searchResults.style.display = 'none';
+      return
+    }
+    
+    searchResults.style.display = 'block';
+
+    axios
+      .get(`/api/search?q=${this.value}`)
+      .then( res => {
+        if(res.data.length) {
+          searchResults.innerHTML = dompurify.sanitize(searchResultsHTML(res.data));
+          return;
+        }
+        searchResults.innerHTML = dompurify.sanitize(`<div class="search__result">No results for <strong>${this.value}</strong> found!</div>`);
+      })
+      .catch( err => {
+        console.log(err);
+        searchResults.innerHTML = dompurify.sanitize(`<div class="search__result">No results found!</div>`);
+      });
+  });
+
+  searchInput.on('keyup', (e) => {
+    console.log(e);
+    if(![38, 40, 13].includes(e.keyCode)){
+      return;
+    }
+    const activeClass = 'search__result--active';
+    const current = search.querySelector(`.${activeClass}`);
+    const items = search.querySelectorAll('.search__result');
+    let next;
+
+    e.preventDefault();
+
+    if (e.keyCode === 40 && current) {
+      next = current.nextElementSibling || items[0];
+    } else if (e.keyCode === 40) {
+      next = items[0];
+    } else if (e.keyCode === 38 && current) {
+      next = current.previousElementSibling || items[items.length - 1];
+    } else if (e.keyCode === 38) {
+      next = items[items.length - 1]
+    } else if (e.keyCode === 13 && current.href) {
+      window.location = current.href;
+    }
+
+    if (current) current.classList.remove(activeClass);
+    next.classList.add(activeClass);
+
+    console.log(next);
+  });
+}
+
+export default typeAhead;
